4bd8c85849ea68535a9bf623cf36ac70f3177e29
kimcoleman <kim@strangerstudios.com>
2018-06-19 13:19:20 -0400

    Adding activation message to plugin

--- pmpro-cpt.php
+++ pmpro-cpt.php
@@ -8,6 +8,8 @@ Author: Paid Memberships Pro
 Author URI: https://www.paidmembershipspro.com/
 */
 
+define( 'PMPRO_CPT_BASENAME', plugin_basename( __FILE__ ) );
+
 function pmprocpt_page_meta_wrapper()
 {
 	$selected_cpts = pmprocpt_getCPTs();
@@ -192,30 +194,68 @@ function pmprocpt_options_page()
 <?php
 }
 
-/*
-Function to add links to the plugin action links
-*/
+/**
+ * Register activation hook.
+ */
+register_activation_hook( PMPRO_CPT_BASENAME, 'pmprocpt_admin_notice_activation_hook' );
+
+/**
+ * Runs only when the plugin is activated.
+ *
+ * @since 0.1.0
+ */
+function pmprocpt_admin_notice_activation_hook() {
+	// Create transient data.
+	set_transient( 'pmprocpt-admin-notice', true, 5 );
+}
+
+/**
+ * Admin Notice on Activation.
+ *
+ * @since 0.1.0
+ */
+function pmprocpt_admin_notice() {
+	// Check transient, if available display notice.
+	if ( get_transient( 'pmprocpt-admin-notice' ) ) { ?>
+		<div class="updated notice is-dismissible">
+			<p><?php printf( __( 'Thank you for activating. <a href="%s">Visit the settings page</a> to get started with the CPT Add On.', 'pmpro-cpt' ), get_admin_url( null, 'options-general.php?page=pmprocpt_options' ) ); ?></p>
+		</div>
+		<?php
+		// Delete transient, only display this notice once.
+		delete_transient( 'pmprocpt-admin-notice' );
+	}
+}
+add_action( 'admin_notices', 'pmprocpt_admin_notice' );
+
+/**
+ * Function to add links to the plugin action links
+ *
+ * @param array $links Array of links to be shown in plugin action links.
+ */
 function pmprocpt_add_action_links($links) {
 	
 	$new_links = array(
-			'<a href="' . get_admin_url(NULL, 'options-general.php?page=pmprocpt_options') . '">Settings</a>',
+			'<a href="' . get_admin_url(NULL, 'options-general.php?page=pmprocpt_options') . '">' . __( 'Settings', 'pmpro-cpt' ) . '</a>',
 	);
-	return array_merge($new_links, $links);
+	return array_merge( $new_links, $links );
 }
 add_filter('plugin_action_links_' . plugin_basename(__FILE__), 'pmprocpt_add_action_links');
 
-/*
-Function to add links to the plugin row meta
-*/
-function pmprocpt_plugin_row_meta($links, $file) {
-	if(strpos($file, 'pmpro-cpt.php') !== false)
-	{
+/**
+ * Function to add links to the plugin row meta
+ *
+ * @param array  $links Array of links to be shown in plugin meta.
+ * @param string $file Filename of the plugin meta is being shown for.
+ */
+function pmprocpt_plugin_row_meta( $links, $file ) {
+	if ( strpos( $file, 'pmpro-cpt.php' ) !== false ) {
 		$new_links = array(
-			'<a href="' . esc_url('https://www.paidmembershipspro.com/add-ons/custom-post-type-membership-access/') . '" title="' . esc_attr( __( 'View Documentation', 'pmpro' ) ) . '">' . __( 'Docs', 'pmpro' ) . '</a>',
-			'<a href="' . esc_url('https://paidmembershipspro.com/support/') . '" title="' . esc_attr( __( 'Visit Customer Support Forum', 'pmpro' ) ) . '">' . __( 'Support', 'pmpro' ) . '</a>',
+			'<a href="' . esc_url( 'https://www.paidmembershipspro.com/add-ons/custom-post-type-membership-access/' ) . '" title="' . esc_attr( __( 'View Documentation', 'pmpro' ) ) . '">' . __( 'Docs', 'pmpro-cpt' ) . '</a>',
+			'<a href="' . esc_url( 'https://www.paidmembershipspro.com/support/' ) . '" title="' . esc_attr( __( 'Visit Customer Support Forum', 'pmpro' ) ) . '">' . __( 'Support', 'pmpro-cpt' ) . '</a>',
 		);
-		$links = array_merge($links, $new_links);
+		$links     = array_merge( $links, $new_links );
 	}
 	return $links;
 }
-add_filter('plugin_row_meta', 'pmprocpt_plugin_row_meta', 10, 2);
+add_filter( 'plugin_row_meta', 'pmprocpt_plugin_row_meta', 10, 2 );
+
